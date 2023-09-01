887b1cedd558b4fa5a0fe2c9b9ba8e9e1ed257d6
IvyZayn <kakapon713@outlook.com>
2023-09-01 21:03:33 +0800

    Added frontend logic to show NFTs.

--- src/opend_assets/src/components/App.jsx
+++ src/opend_assets/src/components/App.jsx
@@ -3,12 +3,18 @@ import Header from "./Header";
 import Footer from "./Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import homeImage from "../../assets/home-img.png";
+import Item from "./Item";
+
 
 function App() {
+
+  const NFTID = "rkp4c-7iaaa-aaaaa-aaaca-cai";
+
   return (
     <div className="App">
       <Header />
-      <img className="bottom-space" src={homeImage} />
+      <Item id={NFTID}/>
+      {/* <img className="bottom-space" src={homeImage} /> */}
       <Footer />
     </div>
   );
--- src/opend_assets/src/components/Item.jsx
+++ src/opend_assets/src/components/Item.jsx
@@ -1,20 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../../assets/logo.png";
+import {Actor, HttpAgent} from "@dfinity/agent";
+import {idlFactory} from "../../../declarations/nft";
+import {Principal} from "@dfinity/principal";
 
-function Item() {
+function Item(props) {
+
+  const [name, setName] = useState("");
+  const [ownerId, setOwnerId] = useState("");
+  const [image, setImage] = useState();
+
+  const id = Principal.fromText(props.id);
+
+  const localHost = "http://localhost:8080/"; // to make HTTP request to fetch the nft canister
+  const agent = new HttpAgent({host: localHost}); // create a HTTP argent
+
+  async function loadNFT() { // create a nft actor and fetch data from backend
+    const NFTActor = await Actor.createActor(idlFactory, {
+      agent,
+      canisterId: id
+    });
+
+    const name = await NFTActor.getName();
+    const owner = await NFTActor.getOwner();
+    const imageBytes = await NFTActor.getImage();
+
+    // turn image bytes to an image URL
+    const imageContent = new Uint8Array(imageBytes);
+    const imageURL = URL.createObjectURL(
+      new Blob([imageContent.buffer], {type: "image/png"})
+    );
+
+    setName(name);
+    setOwnerId(owner.toText());
+    setImage(imageURL);
+  };
+
+
+  useEffect(() => {
+    loadNFT();
+  }, []);
+
   return (
     <div className="disGrid-item">
       <div className="disPaper-root disCard-root makeStyles-root-17 disPaper-elevation1 disPaper-rounded">
         <img
           className="disCardMedia-root makeStyles-image-19 disCardMedia-media disCardMedia-img"
-          src={logo}
+          src={image}
         />
         <div className="disCardContent-root">
           <h2 className="disTypography-root makeStyles-bodyText-24 disTypography-h5 disTypography-gutterBottom">
-            CryptoDunks #312<span className="purple-text"></span>
+            {name}<span className="purple-text"></span>
           </h2>
           <p className="disTypography-root makeStyles-bodyText-24 disTypography-body2 disTypography-colorTextSecondary">
-            Owner: sdfsdf-erwerv-sdf
+            Owner: {ownerId}
           </p>
         </div>
       </div>

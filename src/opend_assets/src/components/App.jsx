d2e823c1825a486a1ce91e08dd5d4e531c27acb0
IvyZayn <kakapon713@outlook.com>
2023-09-02 12:08:49 +0800

    Added feature of routes on the nevigation bar.

--- src/opend_assets/src/components/App.jsx
+++ src/opend_assets/src/components/App.jsx
@@ -2,21 +2,20 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
-import homeImage from "../../assets/home-img.png";
+
 import Item from "./Item";
 import Minter from "./Minter";
 
 
 function App() {
 
-  const NFTID = "rkp4c-7iaaa-aaaaa-aaaca-cai";
+  // const NFTID = "rkp4c-7iaaa-aaaaa-aaaca-cai";
 
   return (
     <div className="App">
       <Header />
-      <Minter />
+      {/* <Minter /> */}
       {/* <Item id={NFTID}/> */}
-      {/* <img className="bottom-space" src={homeImage} /> */}
       <Footer />
     </div>
   );
